840802b8d73964df530e8b3874cd0c55863d3945
Oded Shafran <oded.sh@fiverr.com>
2013-11-11 10:52:58 +0200

    done

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,17 +4,43 @@ describe "Static pages" do
 
   subject { page }
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))# fill in
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))# fill in
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))# fill in
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))# fill in
+  end
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
 
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
     it { should have_content('Sample App') }
     it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Help page" do
     before { visit help_path }
 
+    let(:heading) { 'Help' }
+    let(:title) { 'Help' }
     it { should have_content('Help') }
     it { should have_title(full_title('Help')) }
   end
@@ -31,5 +57,6 @@ describe "Static pages" do
 
     it { should have_content('Contact') }
     it { should have_title(full_title('Contact')) }
+    it { should have_selector('h1', text: 'Contact') }
   end
 end
